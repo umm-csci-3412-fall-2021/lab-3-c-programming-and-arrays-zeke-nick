3c383d1a9a9949108e66b0e95b30ccc4f1f8ee7b
Zeke Krug <krug0102@sentinel.morris.umn.edu>
2021-09-30 11:24:56 -0500

    Created array_merge.c Passing a couple tests.  Tried to move all the values into a single array, and then call mergesort on that array.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,38 @@
 #include "array_merge.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "../mergesort/mergesort.c"
+#include "../mergesort/mergesort.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+  int k, counter;
+  int* intermediate;
+  int* result;
+  for(int i = 0; i<num_arrays; i++)
+  {
+    k = k + sizes[i];
+  }
+
+  intermediate = (int*)calloc(k, sizeof(int));
+  counter = 0;
+  for(int p = 0; p < num_arrays; p++)
+  {
+    int* temp = values[p];
+    for(int q = 0; q < sizes[p]; q++)
+    {
+      intermediate[counter] = temp[q];
+      counter++;
+    }
+  }
+  mergesort(k, intermediate);
+
+  result = (int*)calloc(k+1, sizeof(int));
+  result[0] = k;
+  for(int j = 0; j < k; j++)
+  {
+    result[j+1] = intermediate[j];
+  }
+
+  free(intermediate);
+  return result;
 }
